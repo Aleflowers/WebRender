ae4b31149bcadf1119ee26f97df7768d18c5f6b4
Aleflowers <chavezfloresale@gmail.com>
2024-07-25 19:09:05 -0600

    CUARTA PARTE DEL CODIGO

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#TERCER PARTE DEL CÓDIGO
+#CUARTA PARTE DEL CÓDIGO
 import scipy.stats
 import streamlit as st
 import time
@@ -7,7 +7,7 @@ st.header('Lanzar una moneda')
 
 chart = st.line_chart([0.5])
 
-def toss_coin(n): # función que emula el lanzamiento de una moneda
+def toss_coin(n):
 
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
@@ -30,6 +30,40 @@ start_button = st.button('Ejecutar')
 
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    mean = toss_coin(number_of_trials)
+
+#TERCER PARTE DEL CÓDIGO
+#import scipy.stats
+#import streamlit as st
+#import time
+
+#st.header('Lanzar una moneda')
+
+#chart = st.line_chart([0.5])
+
+#def toss_coin(n): # función que emula el lanzamiento de una moneda
+
+#    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+#    mean = None
+#    outcome_no = 0
+#    outcome_1_count = 0
+
+#    for r in trial_outcomes:
+#        outcome_no +=1
+#        if r == 1:
+#            outcome_1_count += 1
+#        mean = outcome_1_count / outcome_no
+#        chart.add_rows([mean])
+#        time.sleep(0.05)
+
+#    return mean
+
+#number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+#start_button = st.button('Ejecutar')
+
+#if start_button:
+#    st.write(f'Experimento con {number_of_trials} intentos en curso.')
 
 
 #SEGUNDA ETAPA DEL CÓDIGO
